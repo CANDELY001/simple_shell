b0666ff62ddcdfd07515f1134aecd042fcf3228d
CANDELY001 <candely1charifa@gmail.com>
2023-05-12 04:28:46 -0700

    Header

--- shell.h
+++ shell.h
@@ -17,25 +17,27 @@
 #include <ctype.h>
 #include <errno.h>
 
+
 #define STORAGE_SIZE 1024
 extern char **environ;
 
 char *read_input();
 
+
 /*Useful functions*/
 
 char *_strcpy(char *dest, const char *src);
-size_t _strlen(const char *str);
-char *intToString(int num);
+size_t _strlen(const char *s);
+char *intToString(int n);
 int _isdigit(int c);
 int _atoi(const char *s);
 void *_memcpy(void *dest, const void *src, size_t n);
 char *_strcat(char *dest, const char *src);
 int _strcmp(const char *s1, const char *s2);
 char *_strdup(const char *s);
-int _strncmp(const char *st1, const char *st2, size_t n);
-int is_valid(char *str);
-void free_words(char **words, int n);
-char **split_string(char *str, const char *str_separator, int *num_of_words);
+int _strncmp(const char *s1, const char *s2, size_t n);
+int is_valid(char *s);
+void free_words(char **s, int n);
+char **split_string(char *s, const char *separator, int *n);
 
 #endif

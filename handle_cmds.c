f32fcc449c0a4869e67054f42527f469c30c54ab
CANDELY001 <candely1charifa@gmail.com>
2023-05-24 08:35:51 -0700

    $__$

--- handle_cmds.c
+++ handle_cmds.c
@@ -1,5 +1,34 @@
 #include "shell.h"
 /**
+ * handle_multi_cmds_Helper - handle env, setenv..
+ * @arr: the cmds provided
+ * Return: 1 if one of the cmd is found or 0
+ */
+/* BY CHARIFA MASBAHI & NORA JEOUT*/
+int handle_multi_cmds_Helper(char **arr)
+{
+	char *cmd = arr[0];
+
+	if (_strcmp(cmd, "alias") == 0)
+	{
+		insert_existing_aliases();
+		exec_alias(arr);
+		return (1);
+	} else if (_strcmp(cmd, "unalias") == 0)
+	{
+		insert_existing_aliases();
+		exec_alias(arr);
+		return (1);
+	} else if (_strcmp(cmd, "simple_shell") == 0)
+	{
+		char *filename = arr[1];
+
+		run_commands_from_file(filename);
+		return (1);
+	}
+	return (0);
+}
+/**
  * handle_multi_cmds - handle env, setenv, unsetenv cmds, handle cd, alias
  * @arr: the cmds provided
  * @name: program name
@@ -8,7 +37,6 @@
  * @ppt: _______________
  * Return: 1 if one of the cmd is found or 0
  */
-/* BY CHARIFA MASBAHI & NORA JEOUT*/
 int handle_multi_cmds(char **arr, char *ppt, char *name, int cnt, int n)
 {
 	char *cmd = arr[0];
@@ -37,21 +65,8 @@ int handle_multi_cmds(char **arr, char *ppt, char *name, int cnt, int n)
 	{
 		handle_cd(arr);
 		cmd_was_handled = 1;
-	} else if (_strcmp(cmd, "alias") == 0)
+	} else if (handle_multi_cmds_Helper(arr) == 1)
 	{
-		insert_existing_aliases();
-		exec_alias(arr);
-		cmd_was_handled = 1;
-	} else if (_strcmp(cmd, "unalias") == 0)
-	{
-		insert_existing_aliases();
-		exec_alias(arr);
-		cmd_was_handled = 1;
-	} else if (_strcmp(cmd, "simple_shell") == 0)
-	{
-		char *filename = arr[1];
-
-		run_commands_from_file(filename);
 		cmd_was_handled = 1;
 	}
 	return (cmd_was_handled);

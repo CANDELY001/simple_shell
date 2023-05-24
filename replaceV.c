279bcc02c03b5a9e9c7dd8c6804914f58db0a74b
CANDELY001 <candely1charifa@gmail.com>
2023-05-24 10:28:05 -0700

    *__*

--- replaceV.c
+++ replaceV.c
@@ -1,61 +1,97 @@
 #include "shell.h"
 /**
- * replacv2 - function
- * @arr_words: parameter
- * @var: parameter
- * @value: parameter
- * @pos: parameter
+ * replace_exit_code - replaces $? with the exit code value
+ * @word: word to replace in
+ * @v: variable to replace
+ * @value: value to replace with
+ * @pos: position of variable in the word
  */
-void replacv2(char *arr_words, const char *var, const char *value, char *pos)
+void replace_exit_code(char *word, const char *v, const char *value, char *pos)
+{
+	char exit_code_str[EXIT_CODE_STR_SIZE];
+
+	snprintf(exit_code_str, EXIT_CODE_STR_SIZE, "%s", value);
+	size_t new_len = _strlen(word) - _strlen(v) + _strlen(exit_code_str);
+	char *new_word = malloc((new_len + 1) * sizeof(char));
+
+	if (new_word == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	_strncpy(new_word, word, pos - word);
+	new_word[pos - word] = '\0';
+	_strcat(new_word, exit_code_str);
+	_strcat(new_word, pos + _strlen(v));
+
+	free(word);
+	word = new_word;
+}
+
+/**
+ * replace_process_id - replaces $$ with the process ID value
+ * @word: word to replace in
+ * @v: variable to replace
+ * @val: value to replace with
+ * @pos: position of variable in the word
+ */
+void replace_process_id(char *word, const char *v, const char *val, char *pos)
+{
+	char pid_str[PID_STR_SIZE];
+
+	snprintf(pid_str, PID_STR_SIZE, "%s", val);
+	size_t new_len = _strlen(word) - _strlen(v) + _strlen(pid_str);
+	char *new_word = malloc((new_len + 1) * sizeof(char));
+
+	if (new_word == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	_strncpy(new_word, word, pos - word);
+	new_word[pos - word] = '\0';
+	_strcat(new_word, pid_str);
+	_strcat(new_word, pos + _strlen(v));
+
+	free(word);
+	word = new_word;
+}
+
+/**
+ * replace_env_v - replaces $env with its value
+ * @word: word to replace in
+ * @var: variable to replace
+ * @value: value to replace with
+ * @pos: position of variable in the word
+ */
+void replace_env_v(char *word, const char *var, const char *value, char *pos)
 {
 	char *env_value = env_vars(value, environ);
 
 	if (env_value != NULL)
 	{
-		size_t new_len = _strlen(arr_words) - _strlen(var) + _strlen(env_value);
+		size_t new_len = _strlen(word) - _strlen(var) + _strlen(env_value);
 		char *new_word = malloc((new_len + 1) * sizeof(char));
 
 		if (new_word == NULL)
 		{
 			exit(EXIT_FAILURE);
 		}
-		_strncpy(new_word, arr_words, pos - arr_words);
-		new_word[pos - arr_words] = '\0';
-		_strcat(new_word, env_value);
-		free(arr_words);
-		arr_words = new_word;
-	}
-}
-/**
- * replacv1 - function
- * @arr_words: parameter
- * @var: parameter
- * @value: parameter
- * @pos: parameter
- */
-void replacv1(char *arr_words, const char *var, const char *value, char *pos)
-{
-	char pid_str[PID_STR_SIZE];
-	size_t new_len = _strlen(arr_words) - _strlen(var) + _strlen(pid_str);
-	char *new_word = malloc((new_len + 1) * sizeof(char));
 
-	snprintf(pid_str, PID_STR_SIZE, "%s", value);
-	if (new_word == NULL)
-	{
-		exit(EXIT_FAILURE);
+		_strncpy(new_word, word, pos - word);
+		new_word[pos - word] = '\0';
+		_strcat(new_word, env_value);
+		_strcat(new_word, pos + _strlen(var));
+
+		free(word);
+		word = new_word;
 	}
-	_strncpy(new_word, arr_words, pos - arr_words);
-	new_word[pos - arr_words] = '\0';
-	_strcat(new_word, pid_str);
-	_strcat(new_word, pos + _strlen(var));
-	free(arr_words);
-	arr_words = new_word;
 }
 /**
- * replace_variable - function
- * @arr_words: parameter
- * @var: parameter
- * @value: parameter
+ * replace_variable - replaces variables in arr_words with their values
+ * @arr_words: array of words
+ * @var: variable to replace
+ * @value: value to replace with
  */
 void replace_variable(char **arr_words, const char *var, const char *value)
 {
@@ -69,26 +105,16 @@ void replace_variable(char **arr_words, const char *var, const char *value)
 		{
 			if (_strcmp(var, "$?") == 0)
 			{
-				char exit_code_str[EXIT_CODE_STR_SIZE];
-				size_t nln = _strlen(arr_words[i]) - _strlen(var) + _strlen(exit_code_str);
-				char *new_word = malloc((nln + 1) * sizeof(char));
-
-				snprintf(exit_code_str, EXIT_CODE_STR_SIZE, "%s", value);
-				if (new_word == NULL)
-				{
-					exit(EXIT_FAILURE);
-				}
-				_strncpy(new_word, arr_words[i], pos - arr_words[i]);
-				new_word[pos - arr_words[i]] = '\0';
-				_strcat(new_word, exit_code_str);
-				_strcat(new_word, pos + strlen(var));
-				free(arr_words[i]);
-				arr_words[i] = new_word;
+				replace_exit_code(arr_words[i], var, value, pos);
 			}
-			else if (strcmp(var, "$$") == 0)
-				replacv1(arr_words[i], var, value, pos);
+			else if (_strcmp(var, "$$") == 0)
+			{
+				replace_process_id(arr_words[i], var, value, pos);
+			}
 			else if (_strcmp(var, "$") == 0 && _strlen(var) == 1)
-				replacv2(arr_words[i], var, value, pos);
+			{
+				replace_env_v(arr_words[i], var, value, pos);
+			}
 		}
 		i++;
 	}

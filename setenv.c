9cb2916eea83b4834be8e0585e3bf414f49be9dc
CANDELY001 <125865959+CANDELY001@users.noreply.github.com>
2023-05-25 01:23:43 +0100

    Update setenv.c

--- setenv.c
+++ setenv.c
@@ -9,13 +9,13 @@
 /* BY CHARIFA MASBAHI & NORA JEOUT*/
 int _setenv(const char *name, const char *value, int overwrite)
 {
-	if (name == NULL || value == NULL)
-		return (-1);
 	size_t namelen = _strlen(name);
 	size_t value_len = _strlen(value);
 	size_t total_len = namelen + value_len + 2;
 	char *env_var = malloc(total_len);
 
+	if (name == NULL || value == NULL)
+		return (-1);
 	if (env_var == NULL)
 		return (-1);
 	snprintf(env_var, total_len, "%s=%s", name, value);

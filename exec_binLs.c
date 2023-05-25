100d8fd09eb0dfe8b962a88d3ff76eb7e06d1f91
CANDELY001 <125865959+CANDELY001@users.noreply.github.com>
2023-05-25 04:19:04 +0100

    Update exec_binLs.c

--- exec_binLs.c
+++ exec_binLs.c
@@ -7,10 +7,6 @@
 /* BY CHARIFA MASBAHI & NORA JEOUT*/
 void exec_binLs(char **arr_words)
 {
-    int i = 0;
-
-    while (arr_words[i] != NULL)
-    {
 	pid_t pid;
 	int curr_status;
 
@@ -20,13 +16,12 @@ void exec_binLs(char **arr_words)
 		cmd_execve("/bin/ls", arr_words);
 		perror("Error executing command\n");
 		exit(100);
-	} else if (pid < 0)
+	}
+	else if (pid < 0)
 	{
 		perror("Error forking");
 		return;
 	}
-	else
-		waitpid(pid, &curr_status, 0);
-	i++;
-    }
+
+	waitpid(pid, &curr_status, 0);
 }

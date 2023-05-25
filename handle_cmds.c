d43606ae7c4f18808761fa7a770d2996a232f649
norajeout <norajeout123@gmail.com>
2023-05-25 15:19:35 -0700

    updated

--- handle_cmds.c
+++ handle_cmds.c
@@ -1,32 +1,5 @@
 #include "shell.h"
 /**
- * handle_multi_cmds_Helper - handle env, setenv..
- * @arr: the cmds provided
- * Return: 1 if one of the cmd is found or 0
- */
-/* BY CHARIFA MASBAHI & NORA JEOUT*/
-int handle_multi_cmds_Helper(char **arr)
-{
-	char *cmd = arr[0];
-
-	if (_strcmp(cmd, "alias") == 0)
-	{
-		exec_alias(arr);
-		return (1);
-	} else if (_strcmp(cmd, "unalias") == 0)
-	{
-		exec_alias(arr);
-		return (1);
-	} else if (_strcmp(cmd, "simple_shell") == 0)
-	{
-		char *filename = arr[1];
-
-		run_commands_from_file(filename);
-		return (1);
-	}
-	return (0);
-}
-/**
  * handle_multi_cmds - handle env, setenv, unsetenv cmds, handle cd, alias
  * @arr: the cmds provided
  * @name: program name
@@ -35,6 +8,7 @@ int handle_multi_cmds_Helper(char **arr)
  * @ppt: _______________
  * Return: 1 if one of the cmd is found or 0
  */
+/* BY CHARIFA MASBAHI & NORA JEOUT*/
 int handle_multi_cmds(char **arr, char *ppt, char *name, int cnt, int n)
 {
 	char *cmd = arr[0];
@@ -62,9 +36,6 @@ int handle_multi_cmds(char **arr, char *ppt, char *name, int cnt, int n)
 	} else if (_strcmp(cmd, "cd") == 0)
 	{
 		handle_cd(arr);
-		cmd_was_handled = 1;
-	} else if (handle_multi_cmds_Helper(arr) == 1)
-	{
 		cmd_was_handled = 1;
 	}
 	return (cmd_was_handled);

e0b1d857021f536d1dd19c58475b2d4d162387ae
norajeout <norajeout123@gmail.com>
2023-05-25 17:36:28 -0700

    p

--- strcmp.c
+++ strcmp.c
@@ -5,9 +5,11 @@
  * @s2: a parameter
  * Return: result
  */
- /* BY CHARIFA MASBAHI & NORA JEOUT*/
+/* BY CHARIFA MASBAHI & NORA JEOUT*/
 int _strcmp(const char *s1, const char *s2)
 {
+	if (s1 == NULL || s2 == NULL)
+		return -1;
 	while (*s1 == *s2)
 	{
 		if (*s1 == '\0')

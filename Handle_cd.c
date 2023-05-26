e77f865e6b57c6cfed2089a1bef17274ac43dc88
CANDELY001 <125865959+CANDELY001@users.noreply.github.com>
2023-05-26 22:57:46 +0100

    Update Handle_cd.c

--- Handle_cd.c
+++ Handle_cd.c
@@ -1,27 +1,26 @@
 #include "shell.h"
+
 /**
  * handle_cd - handle cd command
- * @arr_words: the arr of words
+ * @arr_words: the array of words
  * Return: void
  */
-/* BY CHARIFA MASBAHI & NORA JEOUT*/
 void handle_cd(char **arr_words)
 {
 	char *initial_directory = NULL, *the_old_pwd, *the_curr_pwd;
 
 	if (arr_words[1] == NULL)
-	{
 		initial_directory = env_vars("HOME", environ);
-	}
 	else if (_strcmp(arr_words[1], "-") == 0)
 	{
 		initial_directory = env_vars("OLDPWD", environ);
 		if (initial_directory == NULL)
 		{
-			isError("OLDPWD not set");
+			perror("cd");
 			return;
 		}
-	} else
+	}
+	else
 		initial_directory = (char *)arr_words[1];
 	the_old_pwd = handle_cwd();
 	if (the_old_pwd == NULL)
@@ -29,16 +28,22 @@ void handle_cd(char **arr_words)
 	if (_setenv("OLDPWD", the_old_pwd, 1) < 0)
 	{
 		free(the_old_pwd);
+		perror("cd");
 		return;
 	}
 	free(the_old_pwd);
-	switch_current_dir(initial_directory);
+	if (switch_current_dir(initial_directory) == -1)
+	{
+		perror("cd");
+		return;
+	}
 	the_curr_pwd = handle_cwd();
 	if (the_curr_pwd == NULL)
 		return;
 	if (_setenv("PWD", the_curr_pwd, 1) < 0)
 	{
 		free(the_curr_pwd);
+		perror("cd");
 		return;
 	}
 	free(the_curr_pwd);

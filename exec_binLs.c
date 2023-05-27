6bac3eb4db0e81b3a8bfd8f61db0a572d607ce5a
CANDELY001 <125865959+CANDELY001@users.noreply.github.com>
2023-05-27 01:45:55 +0100

    Update exec_binLs.c

--- exec_binLs.c
+++ exec_binLs.c
@@ -1,27 +1,35 @@
 #include "shell.h"
+#include <sys/wait.h>
+
 /**
  * exec_binLs - handle /bin/ls
  * @arr_words: arr containing commands
  * Return: void
  */
-/* BY CHARIFA MASBAHI & NORA JEOUT*/
 void exec_binLs(char **arr_words)
 {
-	pid_t pid;
-	int curr_status;
+    pid_t pid;
+    int curr_status;
 
-	pid = fork();
-	if (pid == 0)
-	{
-		cmd_execve("/bin/ls", arr_words);
-		perror("Error executing command\n");
-		exit(100);
-	}
-	else if (pid < 0)
-	{
-		perror("Error forking");
-		return;
-	}
+    pid = fork();
+    if (pid == 0)
+    {
+        cmd_execve("/bin/ls", arr_words);
+        perror("Error executing command\n");
+        exit(2);
+    }
+    else if (pid < 0)
+    {
+        perror("Error forking");
+        return;
+    }
 
-	waitpid(pid, &curr_status, 0);
+    waitpid(pid, &curr_status, 0);
+
+    if (WIFEXITED(curr_status))
+        exit(WEXITSTATUS(curr_status));
+    else if (WIFSIGNALED(curr_status))
+        exit(128 + WTERMSIG(curr_status));
+    else
+        exit(1);
 }
